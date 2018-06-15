0a206c60bb7e9779aca941177905d316a9bf935e
eXway <38926822+eXway@users.noreply.github.com>
2018-06-15 10:43:18 +0200

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -5,12 +5,12 @@ module.exports.run = async (bot, message, args) => {
 let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!hUser) return message.channel.send("Nie podałeś kogo chcesz pogłaskać :c");
 let {body} = await superagent
-.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&tag=pat&fmt=json`);
+.get(`https://api.tenor.com/v1/random?key=88RODP3HJCA2&q=Anime+Head+pat&safesearch=off`);
 
 let hugembed = new Discord.RichEmbed()
 .setColor("#dda6bb")
 .addField("Hug!", `${message.author} pogłaskał(a) ${hUser}`)
-.setImage(body.data.images.original.url);
+.setImage(body.results.url);
 
 message.delete().catch(O_o=>{});
 
