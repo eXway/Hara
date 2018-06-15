dea73e055faf5d9ff84a197daf93c36add771f47
eXway <38926822+eXway@users.noreply.github.com>
2018-06-15 10:20:53 +0200

    pat pat

--- commands/pat.js
+++ commands/pat.js
@@ -1,1 +1,23 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
 
+module.exports.run = async (bot, message, args) => {
+let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!hUser) return message.channel.send("Nie podałeś kogo chcesz pogłaskać :c");
+let {body} = await superagent
+.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&tag=pat&fmt=json`);
+
+let hugembed = new Discord.RichEmbed()
+.setColor("#dda6bb")
+.addField("Hug!", `${message.author} pogłaskał(a) ${hUser}`)
+.setImage(body.data.images.original.url);
+
+message.delete().catch(O_o=>{});
+
+message.channel.send(hugembed);
+
+}
+
+module.exports.help = {
+  name: "pat"
+}
