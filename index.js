b0f077b6ca7a14e315d62daca01a99ca4fced357
eXway <38926822+eXway@users.noreply.github.com>
2018-07-22 17:55:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require("./config.json");
+const key = process.env.token;
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
@@ -109,4 +110,4 @@ if(commandfile) commandfile.run(bot,message,args);
 
 })
 
-bot.login(config.token);
+bot.login(key);
