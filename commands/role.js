50cbd1fc13b5547973b34ce8b8f60f199c2a7956
eXway <38926822+eXway@users.noreply.github.com>
2018-08-06 21:34:07 +0200

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js");
-const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
 
-let roleId = collection.find('role', args[1]);
-return message.channel.send(roleId);
+  //!addrole @andrew Dog Person
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Nie możesz tego zrobić :c");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Nie mogę cię znaleźć");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("Nie podałeś żadnej roli :c");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("Nie mogłem znaleźć tej roli :c");
 
+  if(rMember.roles.has(gRole.id)) return message.reply("Nie mogę tego zrobić, ty już masz tę rolę");
+  await(rMember.addRole(gRole.id));
+
+  try{
+    await rMember.send(`Gratki, dodałeś sobie rolę: ${gRole.name}`)
+  }
 }
 
 module.exports.help = {
