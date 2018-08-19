4f71c34b0038b8acd634db5c5fefd169b8f849c2
eXway <r.okenczyc123@gmail.com>
2018-08-19 16:13:22 +0200

    asdbn

--- commands/role.js
+++ commands/role.js
@@ -5,7 +5,7 @@ let role = args[0];
 if(!role) return message.reply("Podaj rolę! :c");
 let roleID = role.id;
 let fRole = message.guild.roles.find(`name`, role);
-if (!gRole) return message.reply("Nie mogę przydzielić tobie ter roli");
+if (!fRole) return message.reply("Nie mogę przydzielić tobie ter roli");
 
 if(message.user.has(fRole.id));
 await(message.user.addRole(fRole.id));
