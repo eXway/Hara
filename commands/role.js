45e319d80cdecba7a8acf1e9ed515804991f0ce2
eXway <r.okenczyc123@gmail.com>
2018-08-19 16:35:41 +0200

    asd

--- commands/role.js
+++ commands/role.js
@@ -4,13 +4,18 @@ module.exports.run = async (bot, message, args) => {
 let roleUser = message.guild.member(message.author);
 let role = args[0];
 if(!role) return message.reply("Podaj rolę! :c");
-let roleID = role.id;
 let fRole = message.guild.roles.find(`name`, role);
-if (!fRole) return message.reply("Nie mogę przydzielić tobie ter roli");
-
+if (!fRole) return message.reply("Nie mogę przydzielić tobie tej roli");
+if (
+  fRole == "Yaoi"||
+  fRole == "Biedronka"||
+  fRole == "HoneyWorks"||
+  fRole == "MM") {
 await(roleUser.addRole(fRole.id));
-return message.channel.send(`Proszu oto <@${roleID}>, ${roleUser}`);
+return message.channel.send(`Proszu ${roleUser}, dodałeś sobie rolę ${fRole}`);
+}
 
+return message.reply("Nie możesz mieć tej roli :c");
 
 }
 module.exports.help = {
