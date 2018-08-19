cfea645f57ec9eda7b79303d8b2648fe719f1323
eXway <r.okenczyc123@gmail.com>
2018-08-19 16:23:29 +0200

    123456789

--- commands/role.js
+++ commands/role.js
@@ -8,10 +8,8 @@ let roleID = role.id;
 let fRole = message.guild.roles.find(`name`, role);
 if (!fRole) return message.reply("Nie mogę przydzielić tobie ter roli");
 
-if(!roleUser.has(fRole.id)) await(roleUser.addRole(fRole.id));
-
-
-return message.channel.send(`Proszu oto <@${roleID}>`);
+await(roleUser.addRole(fRole.id));
+return message.channel.send(`Proszu oto <@${roleID}>, ${roleUser}`);
 
 
 }
