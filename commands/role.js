7eb23e03624e15d876c98a2edc33bf97ff71eb66
eXway <r.okenczyc123@gmail.com>
2018-08-19 16:42:23 +0200

    asd

--- commands/role.js
+++ commands/role.js
@@ -7,15 +7,15 @@ if(!role) return message.reply("Podaj rolę! :c");
 let fRole = message.guild.roles.find(`name`, role);
 if (!fRole) return message.reply("Nie mogę przydzielić tobie tej roli");
 if (
-  fRole == "Yaoi"||
-  fRole == "Biedronka"||
-  fRole == "HoneyWorks"||
-  fRole == "MM") {
+  fRole.id == "480748055604232232"||
+  fRole.id == "480747941724815362"||
+  fRole.id == "480747845700550679"||
+  fRole.id == "480747654251282433") {
 await(roleUser.addRole(fRole.id));
 return message.channel.send(`Proszu ${roleUser}, dodałeś sobie rolę ${fRole}`);
 }
 
-return message.reply(`Nie możesz mieć tej roli :c ${fRole}`);
+return message.reply(`Nie możesz mieć tej roli :c `);
 
 }
 module.exports.help = {
