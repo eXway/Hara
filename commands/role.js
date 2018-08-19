48d633dc1c3feb0339658685d7d0157c69760f2c
eXway <r.okenczyc123@gmail.com>
2018-08-19 16:01:34 +0200

    asdasd

--- commands/role.js
+++ commands/role.js
@@ -1,8 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let role = message.find('role', args[1]);
+let role = args.join(" ").slice(22);
+if(!role) return message.reply("Podaj rolę! :c");
 let roleID = role.id;
+let fRole = message.guild.roles.find(`name`, role);
+if (!gRole) return message.reply("Nie mogę przydzielić tobie ter roli");
+
+if(message.user.has(fRole.id));
+await(message.user.addRole(fRole.id));
+
+
 return message.channel.send(`Proszu oto <@${roleID}>`);
 
 
